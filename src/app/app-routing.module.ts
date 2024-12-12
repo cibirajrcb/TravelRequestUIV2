b3d15b53255609937cfd4efea7293826321441b8
vickyvin <vickyvin955@gmail.com>
2024-12-13 00:50:02 +0530

    added login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { TravelRequestComponent } from './travel-request/travel-request.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 const routes: Routes = [
+  { path:"", component:LogInComponent},
   { path: "home", component: HomeComponent },
   { path: "user", component: UserComponent },
   { path: "createrequest", component: TravelRequestComponent }
