3fd4c37b1d4e0b7c90c56716cd21cf9598267039
cibirajrcb <rrcibiraj@gmail.com>
2024-12-13 18:27:43 +0500

    changes for showall

--- src/app/travel-request/travel-request.component.ts
+++ src/app/travel-request/travel-request.component.ts
@@ -7,18 +7,17 @@ import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProv
   selector: 'app-travel-request',
   standalone: false,
   templateUrl: './travel-request.component.html',
-  styleUrls: ['./travel-request.component.scss']
+  styleUrls: ['./travel-request.component.scss'],
 })
 export class TravelRequestComponent {
-
   travelForm!: FormGroup;
   public isShowAll = false;
-  public isCreateTravelRequest = false
-  public userData:any= [];
-  public travelRequestData:any= [];
+  public isCreateTravelRequest = false;
+  public userData: any = [];
+  public travelRequestData: any = [];
 
   public departmentsData = Departments;
-  public roleData = Roles
+  public roleData = Roles;
 
   constructor(private fb: FormBuilder, private commonService: CommonService) {}
 
@@ -27,75 +26,66 @@ export class TravelRequestComponent {
       UserID: ['', Validators.required],
       Origin: ['', [Validators.required, Validators.email]],
       Destination: ['', [Validators.required]],
-      StartDate:['', Validators.required],
-      EndDate:['', Validators.required],
-      Purpose:['', Validators.required],
+      StartDate: ['', Validators.required],
+      EndDate: ['', Validators.required],
+      Purpose: ['', Validators.required],
     });
 
-    if(this.userData?.length === 0){
+    if (this.userData?.length === 0) {
       this.getAllUsers();
     }
-
   }
 
   onSubmit(form: FormGroup) {
     console.log('Valid?', form.valid); // true or false
-    console.log('FORM VALUE', form.value)
+    console.log('FORM VALUE', form.value);
     var request = this.createTravelRequest(form);
-    this.commonService.saveTravelRequest(request).subscribe(response => {
-
+    this.commonService.saveTravelRequest(request).subscribe((response) => {
       alert(JSON.stringify(response));
-      alert("Traveler Saved");
-
-    })
+      alert('Traveler Saved');
+    });
   }
 
-  createTravelRequest(form: FormGroup){
+  createTravelRequest(form: FormGroup) {
     let request = {
-      UserID : form.value.UserID,
-      Origin : form.value.Origin,
-      Destination : form.value.Destination,
-      StartDate : form.value.StartDate,
-      EndDate : form.value.EndDate,
-      Purpose : form.value.Purpose,
-      Status : 'InProgress',
-      CreatedBy : "Admin",
-      CreatedDate : new Date()
-
-
-    }
+      UserID: form.value.UserID,
+      Origin: form.value.Origin,
+      Destination: form.value.Destination,
+      StartDate: form.value.StartDate,
+      EndDate: form.value.EndDate,
+      Purpose: form.value.Purpose,
+      Status: 'InProgress',
+      CreatedBy: 'Admin',
+      CreatedDate: new Date(),
+    };
 
     return request;
   }
 
-  Clear(){
+  Clear() {
     this.travelForm.reset();
   }
 
-  AddTravelRequest(){
-    this.isShowAll = true;
-    this.isCreateTravelRequest = false;
-
-  }
-
-  ShowAllTravelRequest(){
+  AddTravelRequest() {
     this.isShowAll = false;
     this.isCreateTravelRequest = true;
-    this.commonService.getAllTravelRequest().subscribe((response:any)=>{
+  }
+
+  ShowAllTravelRequest() {
+    this.isShowAll = true;
+    this.isCreateTravelRequest = false;
+    this.commonService.getAllTravelRequest().subscribe((response: any) => {
       console.log('all user', response);
-      this.travelRequestData = response?.data
+      this.travelRequestData = response?.data;
       console.log('user data', this.travelRequestData);
-    })
-    
+    });
   }
 
-  getAllUsers(){
-    this.commonService.getAllUser().subscribe((response:any)=>{
+  getAllUsers() {
+    this.commonService.getAllUser().subscribe((response: any) => {
       console.log('all user', response);
-      this.userData = response?.data
+      this.userData = response?.data;
       console.log('user data', this.userData);
-    })
+    });
   }
-
-
 }
